c686e755087812a1038a122bf8d6d388280728d8
Tom Miller <c-thomas.miller1@charter.com>
2018-04-11 07:08:57 -0600

    Ensuring the MACs list is proper.

--- controls/5_2_ssh_server_configuration.rb
+++ controls/5_2_ssh_server_configuration.rb
@@ -187,7 +187,7 @@ control 'cis-dil-benchmark-5.2.12' do
   end
 
   ALLOWED_MACS = [
-    'mac-sha2-512-etm@openssh.com',
+    'hmac-sha2-512-etm@openssh.com',
     'hmac-sha2-256-etm@openssh.com',
     'umac-128-etm@openssh.com',
     'hmac-sha2-512',
