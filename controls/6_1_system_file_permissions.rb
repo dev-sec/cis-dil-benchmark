0604da09ffb876f8da2ce3849b7a47b59c2d8efa
Alp Ozcan <alp.ozcan@qantasloyalty.com>
2019-07-19 15:28:31 +1000

    DIADO-3138 Bring the /etc/passwd,group,shadow,... file permissions in line with CIS AL2 Benchmark v1.0.0 recommendations.

--- controls/6_1_system_file_permissions.rb
+++ controls/6_1_system_file_permissions.rb
@@ -83,8 +83,8 @@ control 'cis-dil-benchmark-6.1.3' do
   shadow_files.each do |f|
     describe file(f) do
       it { should exist }
-      it { should be_readable.by 'owner' }
-      it { should be_writable.by 'owner' }
+      it { should_not be_readable.by 'owner' }
+      it { should_not be_writable.by 'owner' }
       it { should_not be_executable.by 'owner' }
       it { should_not be_writable.by 'group' }
       it { should_not be_executable.by 'group' }
@@ -149,8 +149,8 @@ control 'cis-dil-benchmark-6.1.5' do
   gshadow_files.each do |f|
     describe file(f) do
       it { should exist }
-      it { should be_readable.by 'owner' }
-      it { should be_writable.by 'owner' }
+      it { should_not be_readable.by 'owner' }
+      it { should_not be_writable.by 'owner' }
       it { should_not be_executable.by 'owner' }
       it { should_not be_writable.by 'group' }
       it { should_not be_executable.by 'group' }
@@ -179,8 +179,10 @@ control 'cis-dil-benchmark-6.1.6' do
     it { should be_readable.by 'owner' }
     it { should be_writable.by 'owner' }
     it { should_not be_executable.by 'owner' }
+    it { should be_readable.by 'group' }
     it { should_not be_writable.by 'group' }
     it { should_not be_executable.by 'group' }
+    it { should be_readable.by 'other' }
     it { should_not be_writable.by 'other' }
     it { should_not be_executable.by 'other' }
     its(:uid) { should cmp 0 }
@@ -201,9 +203,10 @@ control 'cis-dil-benchmark-6.1.7' do
 
   describe file('/etc/shadow-') do
     it { should exist }
-    it { should be_readable.by 'owner' }
-    it { should be_writable.by 'owner' }
+    it { should_not be_readable.by 'owner' }
+    it { should_not be_writable.by 'owner' }
     it { should_not be_executable.by 'owner' }
+    it { should_not be_readable.by 'group' }
     it { should_not be_writable.by 'group' }
     it { should_not be_executable.by 'group' }
     it { should_not be_readable.by 'other' }
@@ -254,9 +257,10 @@ control 'cis-dil-benchmark-6.1.9' do
 
   describe file('/etc/gshadow-') do
     it { should exist }
-    it { should be_readable.by 'owner' }
-    it { should be_writable.by 'owner' }
+    it { should_not be_readable.by 'owner' }
+    it { should_not be_writable.by 'owner' }
     it { should_not be_executable.by 'owner' }
+    it { should_not be_readable.by 'group' }
     it { should_not be_writable.by 'group' }
     it { should_not be_executable.by 'group' }
     it { should_not be_readable.by 'other' }
