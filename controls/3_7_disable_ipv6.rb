c509b37d1a7e054a02a2119a0cd5925d7ba8804b
Deric Miguel <dmiguel@logicworks.net>
2020-11-10 18:36:00 -0600

    update section 3.7 to v2
    * CIS DIL v2 changes
    * inspec styleguide changes
    
    on-behalf-of: @Logicworks <dmiguel@logicworks.net>
    Co-authored-by: amlodzianowski <adrianmlodzianowski@gmail.com>
    Co-authored-by: gagarin-dev <37323962+gagarin-dev@users.noreply.github.com>
    Signed-off-by: Deric Miguel <dmiguel@logicworks.net>
    Signed-off-by: Dan Rosenbloom<drosenbloom@logicworks.net>
    Signed-off-by: amlodzianowski <amlodzianowski@logicworks.net>
    Signed-off-by: gagarin-dev <ygagarin@logicworks.net>

---
+++ controls/3_7_disable_ipv6.rb
@@ -0,0 +1,35 @@
+#
+# Copyright 2017, Schuberg Philis B.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# author: Kristian Vlaardingerbroek
+
+cis_level = attribute('cis_level')
+
+title '3.7 Disable IPv6'
+
+control 'cis-dil-benchmark-3.7' do
+  title 'Disable IPv6'
+  desc  "Although IPv6 has many advantages over IPv4, not all organizations have IPv6 or dual stack configurations implemented.\n\nRationale: If IPv6 or dual stack is not to be used, it is recommended that IPv6 be disabled to reduce the attack surface of the system."
+  impact 0.0
+
+  tag cis: 'distribution-independent-linux:3.7'
+  tag level: 2
+
+  only_if { cis_level == 2 }
+
+  describe 'cis-dil-benchmark-3.7' do
+    skip 'Not implemented'
+  end
+end
