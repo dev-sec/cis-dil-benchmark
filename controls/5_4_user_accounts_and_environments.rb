82358659cafe6af16c8003fabdc24e896a3babd1
Spencer Owen <spencer.owen@cdw.com>
2022-11-08 13:06:05 -0700

    Expected group
    Signed-off-by: Spencer Owen <spencer.owen@cdw.com>

--- controls/5_4_user_accounts_and_environments.rb
+++ controls/5_4_user_accounts_and_environments.rb
@@ -19,6 +19,9 @@
 
 cis_level = input('cis_level')
 
+expected_group = 'wheel'
+expected_group = 'sudo' if os.name == 'ubuntu'
+
 title '5.4 User Accounts and Environments'
 
 shadow_files = ['/etc/shadow']
@@ -238,7 +241,7 @@ control 'cis-dil-benchmark-5.6' do
     its('content') { should match(/^auth\s+required\s+pam_wheel.so use_uid$/) }
   end
 
-  describe groups.where { name == 'wheel' } do
+  describe groups.where { name == expected_group } do
     it { should exist }
   end
 end
